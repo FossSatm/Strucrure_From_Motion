052a158db1fe7f235b2d9ced64dc0129da585daa
JohnCrabs <johncrabs1995@gmail.com>
2020-04-11 17:38:06 +0300

    Image() image.py create image parameters and functions

--- lib/image.py
+++ lib/image.py
@@ -1,10 +1,10 @@
 import os
 import cv2 as cv
 
-SIFT = "SIFT"
-SURF = "SURF"
-ORB = "ORB"
-AKAZE = "AKAZE"
+FM_SIFT = "SIFT"
+FM_SURF = "SURF"
+FM_ORB = "ORB"
+FM_AKAZE = "AKAZE"
 
 
 class Image:
@@ -22,6 +22,7 @@ class Image:
         self.kp: [] = []
         self.descr: [] = []
         self.kp_ids: [] = []
+        self.kp_size: int = 0
 
         self.camera: Camera = Camera()
 
@@ -53,6 +54,27 @@ class Image:
     def CHANNELS(self):
         return self.channels
 
+    def KEYPOINT_LIST(self):
+        return self.kp
+
+    def KEYPOINT_AT(self, index: int):
+        return self.kp[index]
+
+    def DESCRIPTOR_LIST(self):
+        return self.descr
+
+    def DESCRIPTOR_AT(self, index: int):
+        return self.descr[index]
+
+    def KEYPOINTS_LIST_SIZE(self):
+        return self.kp_size
+
+    def KEYPOINT_IDS_LIST(self):
+        return self.kp_ids
+
+    def KEYPOINT_IDS_AT(self, index: int):
+        return self.kp_ids[index]
+
     def CAMERA_MATRIX(self):
         return self.camera.CAMERA_MATRIX()
 
@@ -60,12 +82,36 @@ class Image:
     # SET IMAGES #
     # ---------- #
 
-    def open_image(self, src: str):
+    def img_open_image(self, src: str):
         self.src = os.path.normpath(src)
         self.dir_src = os.path.dirname(self.src)
         dir_name = os.path.normpath(self.dir)
         dir_name = dir_name.split(os.sep)
         self.dir_name = dir_name[len(dir_name) - 1]
         basename = os.path.splitext(os.path.basename(self.src))
-        self.name = basename[0]
-        self.suffix = basename[1]
+        self.img_name = basename[0]
+        self.img_suffix = basename[1]
+
+    def img_find_features(self, flag=FM_AKAZE):
+        """
+
+        :param flag:
+        :return:
+        """
+        # Choose the Feature Points Extraction method
+        if flag == FM_SIFT:
+            method = cv.xfeatures2d.SIFT_create(nfeatures=F_THRESHOLD, edgeThreshold=10, contrastThreshold=0.1)
+        elif flag == FM_SURF:
+            method = cv.xfeatures2d.SURF_create(hessianThreshold=3000, nOctaves=4, nOctaveLayers=2, upright=0)
+        elif flag == FM_ORB:
+            method = cv.ORB_create()
+        else:
+            method = cv.AKAZE_create()
+
+        img = cv2.imread(self.src, cv2.IMREAD_GRAYSCALE)  # open image as grayscale
+        kp, descr = method.detectAndCompute(img, None)  # detect and compute keypoints
+        self.kp = kp  # set kp to image class
+        self.descr = descr  # set descr to image class
+        self.kp_size = len(self.kp)  # take the size of kp
+        for i in range(0, self.kp_size):  # for all kps
+            self.kp_ids.append(i)  # set an id the index of kp in list
